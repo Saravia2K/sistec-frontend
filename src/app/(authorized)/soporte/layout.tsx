be746a6e69eb4d22c9fc71874fa8a0c221502ff6
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-04-10 10:22:16 -0600

    Support dashboard page

---
+++ src/app/(authorized)/soporte/layout.tsx
@@ -0,0 +1,10 @@
+import { type Metadata } from "next";
+import { type PropsWithChildren } from "react";
+
+export const metadata: Metadata = {
+  title: "SISTEC | Soporte",
+};
+
+export default function SoporteLayout({ children }: PropsWithChildren) {
+  return children;
+}
