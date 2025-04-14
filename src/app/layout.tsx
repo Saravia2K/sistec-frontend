28ac49aa90b849e5511e421f1db28837075bee92
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-04-13 18:03:59 -0600

    Moving theme to lib folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material";
 import { Be_Vietnam_Pro } from "next/font/google";
 import Providers from "./providers";
-import theme from "@/theme";
 
 import "./global.css";
 import styles from "./styles.module.scss";
+import theme from "@/lib/theme";
 
 const beVietnamPro = Be_Vietnam_Pro({
   weight: ["300", "400", "500", "700"],
